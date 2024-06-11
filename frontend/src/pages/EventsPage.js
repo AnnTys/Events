68082676fc209d3431e26a5aacffda32b050cabb
Anna <>
2024-06-11 18:08:03 +0300

    [+] init commit

---
+++ frontend/src/pages/EventsPage.js
@@ -0,0 +1,43 @@
+import { Await, defer, json, useLoaderData } from "react-router-dom";
+
+import EventsList from "../components/EventsList";
+import { Suspense } from "react";
+
+function EventsPage() {
+  const { events } = useLoaderData();
+
+  // if (events.isError) {
+  //   return <p>{events.message}</p>;
+  // }
+
+  // return <>{<EventsList events={events} />}</>;
+  return (
+    <Suspense fallback={<p style={{textAlign: 'center'}}>Loading...</p>}>
+      <Await resolve={events}>
+        {(loadedEvents) => <EventsList events={loadedEvents} />}
+      </Await>
+    </Suspense>
+  );
+}
+
+export default EventsPage;
+
+async function loadEvents() {
+  const response = await fetch("http://localhost:8080/events");
+
+  if (!response.ok) {
+    //return { isError: true, message: 'Could not fetch events'}
+    //throw {message: 'Could not fetch events'}
+    // throw new Response(JSON.stringify({message: 'Could not fetch events'}), {status: 500})
+    throw json({ message: "Could not fetch events" }, { status: 500 });
+  } else {
+    const resData = await response.json();
+    return resData.events;
+  }
+}
+
+export function loader() {
+  return defer({
+    events: loadEvents(),
+  });
+}
